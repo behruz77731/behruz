f50d4148c1b4f2906f8af0e538429f3cdef6c72a
behruz77731 <sekrtubehruz@gmail.com>
2025-06-13 17:16:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function buyNow() {
+  alert("Mahsulot sotib olindi!");
+}
+
+function buyNow() {
+  alert("Mahsulot sotib olindi!");
+}
+
+function buyProduct(productName) {
+  alert("Siz " + productName + " ni sotib oldingiz!");
+}
